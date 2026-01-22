0878fdecefb531666597332963bdeedda4cc45d4
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 18:12:33 -0500

    Ejemplo con v-if

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   // Componentes
   import defineProps from './components/defineProps.vue'
   import vFor from './components/v-for.vue';
+  import VIf from './components/v-if.vue';
 </script>
 
 <template>
   <!-- Plantilla para visualizar cada componente -->
   <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
   <vFor />
-  
+  <VIf />
 </template>
 
 <style scoped>
---
+++ src/components/v-if.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const lista = [1,2,3,4,5,6,7,8,9,10]
+
+</script>
+
+<template>
+    <div v-if="lista.length >= 9">
+        <h3>Mi Lista de Números:</h3>
+
+        <ul>
+            <li v-for="item in lista" :key="item">
+                Número: {{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
