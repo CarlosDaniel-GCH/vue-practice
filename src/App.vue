b1a3fff9c7b6468e517af2cd1bc43db5178455bf
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 18:30:09 -0500

    Ejemplo simple con @click

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-  // Componentes
-  import defineProps from './components/defineProps.vue'
-  import vFor from './components/v-for.vue';
-  import VIf from './components/v-if.vue';
-  import VModel from './components/v-model.vue'
-  import Ref from './components/ref.vue';
+// Componentes
+import defineProps from './components/defineProps.vue'
+import vFor from './components/v-for.vue';
+import VIf from './components/v-if.vue';
+import VModel from './components/v-model.vue'
+import Ref from './components/ref.vue';
+import Click from './components/click.vue';
 </script>
 
 <template>
@@ -12,8 +13,9 @@
   <!-- <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
   <vFor />
   <VIf />
-  <VModel /> -->
-  <Ref />
+  <VModel />
+  <Ref /> -->
+  <Click />
 </template>
 
 <style scoped>
---
+++ src/components/click.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function incrementar(){
+    contador.value++
+}
+
+</script>
+
+<template>
+    <button @click="incrementar">
+        {{ contador }}
+    </button>
+</template>
