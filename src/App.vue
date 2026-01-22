8d0dd0235d43df6456c22d4aedef7babe2e6cc1e
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 18:03:41 -0500

    Ejemplo con v-for

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-import defineProps from './components/defineProps.vue'
+  // Componentes
+  import defineProps from './components/defineProps.vue'
+  import vFor from './components/v-for.vue';
 </script>
 
 <template>
+  <!-- Plantilla para visualizar cada componente -->
   <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
+  <vFor />
+  
 </template>
 
 <style scoped>
---
+++ src/components/v-for.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const lista = ["Primer Elemento", "Segundo Elemento", "Tercer Elemento", "Cuarto Elemento", "Quinto Elementos"]
+</script>
+
+<template>
+    <div v-for="item in lista">
+        {{ item }}
+    </div>
+</template>
