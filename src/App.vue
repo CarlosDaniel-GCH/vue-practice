6c4d738a07eb5727fe257991790bb34d69a30bbf
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 17:57:58 -0500

    Instalando Vue y agregando defineProps

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import defineProps from './components/defineProps.vue'
+</script>
+
+<template>
+  <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/defineProps.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: String,
+  user: {
+    nombres: String,
+    apellidos: String,
+    edad: Number
+  }
+})
+</script>
+
+<template>
+  <div>
+    <span>{{ msg }}</span>
+    <h1>{{ user.nombres }}</h1>
+    <h2>{{ user.apellidos }}</h2>
+    <h3>{{ user.edad }} años</h3>
+  </div>
+</template>
+
+<style scoped>
+</style>
