c4c188b74c5e9d9817ad6d403c4c1a484dcfdb43
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 18:26:44 -0500

    Ejemplo usando ref y onMounted

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
   import vFor from './components/v-for.vue';
   import VIf from './components/v-if.vue';
   import VModel from './components/v-model.vue'
+  import Ref from './components/ref.vue';
 </script>
 
 <template>
   <!-- Plantilla para visualizar cada componente -->
-  <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
+  <!-- <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
   <vFor />
   <VIf />
-  <VModel />
+  <VModel /> -->
+  <Ref />
 </template>
 
 <style scoped>
---
+++ src/components/ref.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const usuarios = ref([])
+
+const lista = ["user1", "user2", "user3", "user4"]
+
+onMounted(() => {
+    usuarios.value.push(lista)
+})
+
+</script>
+
+<template>
+    <div v-for="user in usuarios">
+        {{ user }}
+    </div>
+</template>
