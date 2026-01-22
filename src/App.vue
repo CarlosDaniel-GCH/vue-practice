abdc42d76e3026c2a822574deb0e4c78670a025d
CarlosDaniel-090304 <carlosdanielchilcon19@gmail.com>
2026-01-22 18:51:43 -0500

    Ejemplo con v-bind

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,18 @@ import VIf from './components/v-if.vue';
 import VModel from './components/v-model.vue'
 import Ref from './components/ref.vue';
 import Click from './components/click.vue';
+import vBind from './components/v-bind.vue';
 </script>
 
 <template>
-  <!-- Plantilla para visualizar cada componente -->
+  <!-- Descomenta el componente que quieras utilizar -->
   <!-- <defineProps msg="Esto es un mensaje" :user="{nombres : 'Pedro' , apellidos : 'Sanchez', edad: 43}" />
   <vFor />
   <VIf />
   <VModel />
-  <Ref /> -->
-  <Click />
+  <Ref />
+  <Click /> -->
+  <vBind />
 </template>
 
 <style scoped>
---
+++ src/components/v-bind.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from 'vue'
+
+const usuario = reactive({
+    id: 101,
+    nombre: "Alex",
+    apellido: "Rivera",
+    estaActivo: true
+})
+
+</script>
+
+<template>
+    <!-- Tambien se puede usar " : " para abreviar en vez de v-bind -->
+    <div>
+        <span v-bind:key="usuario.id">
+            {{ usuario.id }}
+        </span>
+        <h1>{{ usuario.nombre }} {{ usuario.apellido }}</h1>
+        <span>
+            Activo: {{ usuario.estaActivo }}
+        </span>
+    </div>
+</template>
